4b918d91c20620c3861dee4b67650b3300da46f1
DrF33lg00d <Dr.F33lg00d@yandex.ru>
2025-03-05 22:25:07 +1000

    Replace os to pathlib in settings

--- utils/settings.py
+++ utils/settings.py
@@ -1,6 +1,5 @@
 import logging
 import logging.config
-import os
 from pathlib import Path
 from contextlib import suppress
 from datetime import timedelta
@@ -9,17 +8,18 @@ import yaml
 
 
 class InfoFilter(logging.Filter):
-    def filter(self, record: object) -> bool:
+    def filter(self, record: logging.LogRecord) -> bool:
         return record.levelno == logging.INFO
 
 
 class WarnFilter(logging.Filter):
-    def filter(self, record: object) -> bool:
+    def filter(self, record: logging.LogRecord) -> bool:
         return record.levelno >= logging.WARN
 
 
 def setup_logging() -> None:
-    with open(os.path.join(os.getcwd(), "utils", "logging.yaml"), "r") as f:
+    log_config_path = Path(__file__).parent / "logging.yaml"
+    with log_config_path.open() as f:
         log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
     return None
@@ -39,4 +39,4 @@ DB_PATH = Path(__file__).parent.parent / "db.db"
 WORDS: tuple[str] = ("блин",)
 
 with suppress(ImportError):
-    from utils.local_settings import *
+    from utils.local_settings import *  # noqa F403
