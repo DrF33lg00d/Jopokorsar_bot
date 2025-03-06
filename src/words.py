a01a37393a137a346f2d500dae82dd216bf6d32f
DrF33lg00d <Dr.F33lg00d@yandex.ru>
2025-03-06 11:52:36 +1000

    Move cancel operation to words and save word to db

--- src/words.py
+++ src/words.py
@@ -1,9 +1,10 @@
-from aiogram import Router, F
-from aiogram.filters import Command
-from aiogram.types import Message
-from aiogram.fsm.state import StatesGroup, State
+from aiogram import F, Router
+from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import StateFilter
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import Message
+
+from src.db import Chat, get_or_create
 from utils.settings import get_logger
 
 logger = get_logger()
@@ -15,22 +16,37 @@ class BanWordOperation(StatesGroup):
     removing_banword = State()
 
 
+@router.message(StateFilter(*BanWordOperation.__state_names__), Command("cancel"))
+@router.message(
+    StateFilter(*BanWordOperation.__state_names__), F.text.lower() == "отмена"
+)
+async def cmd_cancel(message: Message, state: FSMContext) -> None:
+    if await state.get_state() is None:
+        await message.answer("Нечего отменять")
+        logger.debug("Nothing to cancel")
+    else:
+        await state.clear()
+        await message.answer("Понял, отмена")
+        logger.debug("Cancel operation, set state 'None'")
+
+
 @router.message(Command("add_banword"), StateFilter(None))
 async def cmd_add_banword(message: Message, state: FSMContext):
     await message.answer("Какое слово добавляем?")
     await state.set_state(BanWordOperation.adding_banword)
+    await state.set_data({"chat": get_or_create(message.chat.id)[0]})
     logger.debug("Going to adding banword, set state 'adding_banword'")
-
-
-@router.message(StateFilter(BanWordOperation.__state_names__), Command("cancel"))
-async def cmd_cancel(message: Message, state: FSMContext):
-    await message.answer("Понял, отмена")
-    await state.set_state()
-    logger.debug("Cancel operation, set state 'None'")
 
 
 @router.message(BanWordOperation.adding_banword, F.text)
 async def add_banword(message: Message, state: FSMContext):
-    await message.answer("Триггер-слово настроено")
-    await state.set_state()
-    logger.debug("Banword '%s' added, set state 'None'", message.text.lower())
+    state_data = await state.get_data()
+    chat: Chat = state_data["chat"]
+    is_word_added = chat.add_banword(message.text)
+    if is_word_added:
+        await message.answer("Слово добавлено")
+        logger.debug("Banword '%s' added, set state 'None'", message.text.lower())
+    else:
+        await message.answer("Ошибка при добавлении слова")
+        logger.debug("Banword '%s' not added, set state 'None'", message.text.lower())
+    await state.clear()
