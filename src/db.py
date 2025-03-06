35488418bab65afaae177705f471ea356f9a3f8f
DrF33lg00d <Dr.F33lg00d@yandex.ru>
2025-03-06 11:51:56 +1000

    Add ability to add banword

--- src/db.py
+++ src/db.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, create_engine, select, ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
+from sqlalchemy import DateTime, ForeignKey, String, create_engine, select
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
 
 from utils.settings import DB_PATH, get_logger, setup_logging
 
-
 setup_logging()
 logger = get_logger()
+ENGINE = create_engine(f"sqlite:///{DB_PATH}", echo=True)
+SESSION = Session(ENGINE)
 
 
 class Base(DeclarativeBase):
@@ -24,20 +25,32 @@ class Chat(Base):
     def __repr__(self) -> str:
         return f"Chat(id={self.id!r}, datetime={self.datetime_stamp!r})"
 
+    def add_banword(self, word: str) -> bool:
+        word_instance = BanWord(text=word.lower(), chat=self)
+        with Session(ENGINE) as session:
+            try:
+                session.add(word_instance)
+                session.commit()
+                logger.debug("Created new word: %s", word_instance)
+                return True
+            except IntegrityError:
+                session.rollback()
+                logger.debug("Already exists in DB")
+            except Exception as e:
+                session.rollback()
+                logger.warning("%s - %s", e.__class__.__name__, e.__context__)
+        return False
+
 
 class BanWord(Base):
     __tablename__ = "ban_word"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
-    text: Mapped[str] = mapped_column(String())
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    text: Mapped[str] = mapped_column(String(), unique=True)
     chat_id: Mapped[int] = mapped_column(ForeignKey("chat.id"))
     chat: Mapped["Chat"] = relationship(back_populates="words")
 
     def __repr__(self) -> str:
         return f"Word '{self.text}'"
-
-
-ENGINE = create_engine(f"sqlite:///{DB_PATH}", echo=True)
-SESSION = Session(ENGINE)
 
 
 def init_tables() -> None:
